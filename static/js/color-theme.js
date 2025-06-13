4b34f3ab3e6188804bacc141f8ba7e5eb92e1b45
Torsten Sommer <torsten.sommer@3ds.com>
2025-06-13 10:36:12 +0200

    Update images based on color theme

--- static/js/color-theme.js
+++ static/js/color-theme.js
@@ -1,22 +1,70 @@
 window.addEventListener('DOMContentLoaded', () => {
-    
-    var theme = localStorage.getItem('theme');  
-    
+
+    var theme = localStorage.getItem('theme');
+
     if (theme != 'light' && theme != 'dark') {
         theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
         localStorage.setItem('theme', theme);
     }
-    
+
     document.documentElement.setAttribute('data-bs-theme', theme);
+
+    updateTheme(theme);
 })
 
-function toggleTheme() {
-    
-    var currentTheme = document.documentElement.getAttribute('data-bs-theme');
-    
-    var newTheme = currentTheme == 'dark' ? 'light' : 'dark';
-    
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', event => {
+    const newColorScheme = event.matches ? "dark" : "light";
+    updateTheme(newColorScheme);
+});
+
+function updatePictureSrc(picture, theme) {
+
+    const sources = picture.querySelectorAll('source');
+
+    sources.forEach(source => {
+        
+        const media = source.getAttribute('media');
+
+        dataTheme = source.getAttribute('data-theme');
+
+        if (!dataTheme && media) {
+            if (media.includes('prefers-color-scheme: dark')) {
+                source.setAttribute('data-theme', 'dark');
+                source.setAttribute('media', 'not all');
+            } else if (media.includes('prefers-color-scheme: light')) {
+                source.setAttribute('data-theme', 'light');
+                source.setAttribute('media', 'not all');
+            }
+        }
+
+        dataTheme = source.getAttribute('data-theme');
+
+        if (dataTheme) {
+            if (dataTheme === theme) {
+                source.setAttribute('media', '(prefers-color-scheme: light),(prefers-color-scheme: dark)');
+            } else {
+                source.setAttribute('media', 'not all');
+            }
+        }
+    });
+}
+
+function updateTheme(newTheme) {
+
     localStorage.setItem('theme', newTheme);
-    
+
     document.documentElement.setAttribute('data-bs-theme', newTheme);
+
+    document.querySelectorAll('picture').forEach(picture => {
+        updatePictureSrc(picture, newTheme);
+    });
+}
+
+function toggleTheme() {
+
+    var currentTheme = document.documentElement.getAttribute('data-bs-theme');
+
+    var newTheme = currentTheme == 'dark' ? 'light' : 'dark';
+
+    updateTheme(newTheme);
 }
